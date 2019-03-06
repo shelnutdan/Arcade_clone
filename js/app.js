ede3a9a5fb22cc760331641e4bf50818abc070e2
shelnutdan <shelnutdaniel@gmail.com>
2019-03-05 21:10:10 -0800

    First Commit: Set initial location for enemy, set speed and update method. Got Enemy to render but it does not update. Player class still doe not render

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(xloc,yloc, movement) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x=xloc;
+    this.y=yloc;
+    this.movement=movement
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.movement*dt;
+    if (this.x>500){
+      this.x=-100;
+      this.movement=200;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +32,43 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player=function(xloc,yloc,movement){
+  this.x=xloc;
+  this.y=yloc;
+  this.movement=movement
+  this.sprite='image/char-boy.png';
+}
+Player.prototype.update= function(){
+  if (this.y>380){
+    this.y=380;
+  }
+  if (this.x>400){
+    this.x=400
+  }
+  if(this.x<0){
+    this.x=0
+  }
+  if (this.y<0){
+    this.x=200;
+    this.y=380;
+  }
+}
 
-
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies=[];
 
-
+let enemyPosition=[60,140,220];
+let enemy;
+let player= new Player(200,380,50)
+for (let i=0;i<enemyPosition.length;i++){
+  enemy=new Enemy(0,enemyPosition[i],200);
+  allEnemies.push(enemy)
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
