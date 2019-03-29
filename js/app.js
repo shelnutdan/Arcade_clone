5a9b4d4882fa907a8ba4598d4118e19170c8c5ea
shelnutdan <32445077+shelnutdan@users.noreply.github.com>
2019-03-29 12:39:13 -0700

    Update app.js
    Fixed the level up function and added comment to code

--- js/app.js
+++ js/app.js
@@ -1,30 +1,29 @@
+/*Pauses the game so that player can't move and no enemies render*/
 var paused = true;
 
 $(document).ready(function(){
         $("#gameOver").hide();
-        // Hide the start screen on button click
+        // Hide the start screen
         $("#playGame").click(function(){
-            //Hide the start screen
+
             $("#startScreen").fadeOut('fast');
 
-            /* Unpause the game to allow the player to move around
-            * when arrow keys are pressed
-            */
+            /* Unpause the game to allow the player to move around and eventually causes the enemies to render on the screen*/
+
             paused = false;
 
         });
-        // Hide the game over screen on button click
+        // Hides the game over dsiaply and allows for the game to start again
         $("#playAgain").click(function(){
 
-            // Hide the game over screen
+
             $("#gameOver").hide();
+          //Reinitializes the enemies on the screen
             for (let i=0;i<enemyPosition.length;i++){
               enemy=new Enemy(0,enemyPosition[i],200);
               allEnemies.push(enemy)}
               player.lives=3;
-            /* Unpause the game to allow the player to move around
-            * when arrow keys are pressed
-            */
+
             paused = false;
         });
 
@@ -61,7 +60,7 @@ Enemy.prototype.update = function(dt) {
       player.y=300;
 
       player.lives--;
-      //gameOver();
+
       console.log(player.lives)
       if(player.lives==0){
         gameOver()
@@ -69,7 +68,7 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-
+/*Game over function that trggeres when the player loses its three lives*/
 function gameOver(){
   paused = true;
   $("#gameOver").show();
@@ -118,7 +117,7 @@ Player.prototype.update= function(){
   }
 }
 
-
+/*How the platyer handles key down events from the user*/
 Player.prototype.handleInput=function(key){
   if (key=='left'){
     this.x -=this.movement+50;
@@ -133,6 +132,7 @@ Player.prototype.handleInput=function(key){
     this.y +=this.movement-30;
   }
 }
+/*Renders player image*/
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -155,13 +155,12 @@ for (let i=0;i<enemyPosition.length;i++){
 
 }
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/*Initializes the stage for the user*/
 var Stage =function(){
   this.stage=1;
 
 }
-
+/*updates stage when a player reached the water images, updates current score, and renders more enemies when you reach every five levels. Additionally, the player gets an extra life after a every tenth level.*/
 Stage.prototype.update = function(){
   this.stage+=1;
   stats.currentScore+=1000;
@@ -175,7 +174,7 @@ Stage.prototype.update = function(){
       allEnemies.push(enemy)
 
     }
-  }else if (this.stage%10==0){
+  }if (this.stage%10==0){
     player.lives++;
 
   }else if(player.lives==0){
@@ -188,6 +187,8 @@ Stage.prototype.update = function(){
 /*Initialize a new stage*/
 var stage=new Stage();
 
+
+/*Creates the stat for the game: lives, score, and stages*/
 var Stats = function(){
   this.currentScore=0;
   this.currentStage=stage.stage;
@@ -195,17 +196,16 @@ var Stats = function(){
 
 }
 
-// Render the stat bar, level text , score text , lives count and gems count
+// Render the stat bar, stage text , score text , lives count
 Stats.prototype.render = function() {
     ctx.fillStyle = 'rgba(0,0,0,0.5)';
     ctx.fillRect(0,50,707,45);
     this.stage();
     this.score();
-    //this
     this.lives();
 
 };
-// Level text
+// Stage text
 Stats.prototype.stage = function() {
         ctx.font = 'san-serif';
         ctx.fillStyle = 'white';
@@ -232,23 +232,24 @@ Stats.prototype.score = function(){
 Stats.prototype.updateScore=function(){
   this.currentScore+=1000;
 }
-
+//Live text
 Stats.prototype.lives = function(){
-    //ctx.drawImage(Resources.get('images/stat-heart.png'),340,62);
+    //ctx.drawImage(Resources.get('./images/Heart.png'),340,62);
     ctx.font = 'san-serif';
     ctx.fontStyle = 'red';
     ctx.textAlign = 'center';
     ctx.fillText('Lives '+ player.lives, 250,82);
 
 }
-
+//updates lives text
 Stats.prototype.updateLives = function(lives){
             this.currentLives = lives;
 
 };
-
+//Calls stats object
 var stats = new Stats()
-console.log(stats.currentLives)
+//console.log(stats.currentLives)
+/*Event listener for how the player will react to new inputs*/
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
